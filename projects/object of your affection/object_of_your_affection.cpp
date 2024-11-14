353d8570049264af6b5eebbc14d999ae4ea6f403
Brian P <brian7terra@gmail.com>
2024-11-14 16:24:48 -0700

    Start of project OOYA

---
+++ projects/object of your affection/object_of_your_affection.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+The Object of Your Affection (OOYA)
+
+Requirements:
+1. Build a profile class
+    a. Stores user: Name, age, city, country, pronouns, and hobbies
+
+2. Users will:
+    a. Create profile with some info
+    b. Add hobbies
+    c. View profile
+
+*/
+
+int main () {
+    
+}
